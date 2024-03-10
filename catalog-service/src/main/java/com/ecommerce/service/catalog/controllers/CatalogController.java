e6ec6a291fa0fa450986e4d7f80ea9c5fd2ffb23
osinnowo <liquidcoding2009@gmail.com>
2024-03-10 17:29:45 +0100

    Configuration-Service - Project Setup

--- catalog-service/src/main/java/com/ecommerce/service/catalog/controllers/CatalogController.java
+++ catalog-service/src/main/java/com/ecommerce/service/catalog/controllers/CatalogController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.service.catalog.controllers;
 
 import static com.ecommerce.service.catalog.model.common.BaseResponse.*;
+
+import com.ecommerce.service.catalog.configuration.AppConfiguration;
 import com.ecommerce.service.catalog.model.common.BaseResponse;
 import com.ecommerce.service.catalog.model.dto.ProductCategoryDto;
 import com.ecommerce.service.catalog.service.ProductService;
@@ -19,11 +21,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CatalogController {
 
+    private final AppConfiguration configuration;
     private final ProductService productService;
     @GetMapping("/categories")
     public Mono<ResponseEntity<BaseResponse<List<ProductCategoryDto>>>> categories() {
         return productService.getAllCategories(10)
                 .collectList()
                 .map(item -> okResponse(item));
+    }
+
+    @GetMapping("/configuration")
+    public Mono<ResponseEntity<BaseResponse<AppConfiguration>>> configuration() {
+        return Mono.just(okResponse(configuration));
     }
 }
