3b8276de6de05d6126d3fe78cfca8f8703ca1eca
osinnowo <liquidcoding2009@gmail.com>
2024-03-10 12:28:11 +0100

    Catalog-Service - Profiles & Docker-Compose

--- catalog-service/src/main/java/com/ecommerce/service/catalog/controllers/CatalogController.java
+++ catalog-service/src/main/java/com/ecommerce/service/catalog/controllers/CatalogController.java
@@ -1,10 +1,16 @@
 package com.ecommerce.service.catalog.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/catalog")
 public class CatalogController {
-    
+
+    @GetMapping("/greeting")
+    public Mono<String> greeting() {
+        return Mono.just("Hello, World!");
+    }
 }
