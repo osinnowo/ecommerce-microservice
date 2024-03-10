752c6481935db353ecba0ea1f84b9873ecd18a37
osinnowo <liquidcoding2009@gmail.com>
2024-03-10 11:59:02 +0100

    Catalog-Service - Project Setup (Files & Folder)

---
+++ catalog-service/src/main/java/com/ecommerce/service/catalog/app/AppExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ecommerce.service.catalog.app;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
---
+++ catalog-service/src/main/java/com/ecommerce/service/catalog/controllers/CatalogController.java
@@ -0,0 +1,10 @@
+package com.ecommerce.service.catalog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/catalog")
+public class CatalogController {
+    
+}
